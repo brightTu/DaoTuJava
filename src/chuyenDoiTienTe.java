d8caeb20328d645320d0aeefbd89a2d6c16b9a6e
Tudq <tudq@avadagroup.com>
2025-11-06 14:23:32 +0700

    small

---
+++ src/chuyenDoiTienTe.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class chuyenDoiTienTe {
+    static void main() {
+        // Tỷ giá cố định
+        final double TY_GIA = 23500;
+
+        // Tạo đối tượng Scanner để nhập dữ liệu
+        Scanner Nhap = new Scanner(System.in);
+
+        // Nhập số USD
+        System.out.print("Nhập số tiền (USD): ");
+        double usd = Double.parseDouble(Nhap.nextLine());
+
+        // Tính ra số tiền VND
+        double vnd = usd * TY_GIA;
+
+        // Xuất kết quả
+        System.out.printf("%.2f USD = %, .0f VND\n", usd, vnd);
+
+        // Đóng Scanner
+        Nhap.close();
+    }
+}
